c839067a37d3edf6bdcfa4ffa503dde8789c8a7d
Horia <horia.marinescu12@gmail.com>
2022-11-19 20:48:02 +0200

    //Log in

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -67,7 +67,10 @@ export default function Login() {
                         Forget Password?
                     </a>
                     <div className="mt-6">
-                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                        <button 
+                            onClick={() => signIn()}
+                            className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                            
                             Login
                         </button>
                     </div>
